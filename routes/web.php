42ac27baba4162898159001fb23a33f3c2f44103
sanae.takeda <sa.takeda@ntt.com>
2021-02-08 07:20:11 +0000

    validation

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('Tasks', 'TasksController');
+
+// メッセージの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+Route::post('tasks', 'TasksController@store');
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+Route::put('tasks/{id}', 'TasksController@update');
+// メッセージを削除
+Route::delete('tasks/{id}', 'TasksController@destroy');
