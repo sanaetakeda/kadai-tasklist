b66f3820eca208ac86d80ffd37f58e158559c1a8
sanae.takeda <sa.takeda@ntt.com>
2021-02-12 07:33:33 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,8 +46,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        // メッセージを作成
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
+        // タスクを作成
         $task = new Task;
+        $task->status = $request->staus;
         $task->content = $request->content;
         $task->save();
 
@@ -98,9 +103,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+         // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
